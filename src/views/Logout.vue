cb1e8f07ef0359475e6eb40bf8fe55134ed1a766
fadadmin <walley@walley.org>
2026-05-20 12:42:01 +0200

    logout

---
+++ src/views/Logout.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container class="fill-height d-flex flex-column justify-center align-center">
+    <v-card class="pa-6" max-width="400">
+      <v-card-title class="text-h5">Odhlášení</v-card-title>
+
+      <v-card-text>
+        <p>Byl jste úspěšně odhlášen.</p>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="primary" block @click="$router.push('/login')">
+          Přihlásit se znovu
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Logout",
+};
+</script>
